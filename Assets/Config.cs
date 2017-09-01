567130067f0735b58f85b1556e39a1fc4a47ac08
pontura <pontura@gmail.com>
2017-09-01 17:37:11 -0300

    project created

---
+++ Assets/Config.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.IO;
+using SimpleJSON;
+
+public class Config : MonoBehaviour
+{
+	public string url;
+	public Vector2 limits = new Vector2 (8, 30);
+
+	void Start()
+	{
+		StartCoroutine (LoadData ());
+	}
+	IEnumerator LoadData()
+	{
+		string directory = "file://" + Application.dataPath + "/../" + "settings.json";
+		WWW www = new WWW(directory);
+		yield return www;
+		LoadDataromServer( www.text);
+	}
+	public void LoadDataromServer(string json_data)
+	{
+		var Json = SimpleJSON.JSON.Parse(json_data);
+		fillArray(Json);
+	}
+	private void fillArray(JSONNode content)
+	{
+		url = content[0]["url"];
+		Events.Log (url);
+		Events.OnSettingsLoaded ();
+	}
+}
---
+++ Assets/Data.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using System;
+
+public class Data : MonoBehaviour
+{
+	public builds build;
+	public enum builds
+	{
+		RELEASE,
+		DEBUG
+	}
+
+    const string PREFAB_PATH = "Data";    
+    static Data mInstance = null;
+
+	[HideInInspector]
+	public Config config;
+
+    public static Data Instance
+    {
+        get
+        {
+            if (mInstance == null)
+            {
+                mInstance = FindObjectOfType<Data>();
+            }
+            return mInstance;
+        }
+    }
+    public string currentLevel;
+    public void LoadLevel(string aLevelName)
+    {
+        this.currentLevel = aLevelName;
+        SceneManager.LoadScene(aLevelName);
+    }
+    void Awake()
+    {
+		QualitySettings.vSyncCount = 1;
+
+        if (!mInstance)
+            mInstance = this;
+        else
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+       
+        DontDestroyOnLoad(this.gameObject);
+
+		config = GetComponent<Config> ();
+    }
+
+}
