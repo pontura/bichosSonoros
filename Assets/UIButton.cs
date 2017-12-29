f0169e07b6f2688eb24c22e116748c6aeb21cc04
Diex <diegoalberti@qb9.net>
2017-12-29 13:55:13 -0300

    fix

---
+++ Assets/UIButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIButton : MonoBehaviour {
+
+	public Button b;
+	// Use this for initialization
+	void Start () {
+		b.colors.normalColor = Data.Instance.config.GetBicho (Data.Instance.bichoID).colors[0];
+		b.colors.highlightedColor = Data.Instance.config.GetBicho (Data.Instance.bichoID).colors[1];
+		b.colors.colorMultiplier = 1.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/UIRecording.cs
+++ Assets/UIRecording.cs
@@ -10,7 +10,6 @@ public class UIRecording : MonoBehaviour {
 	public GameObject button;
 	public Text label;
 
-//	public MicRecorder micRecorder;
 	bool isRecording;
 	private GameController context;
 
@@ -41,31 +40,16 @@ public class UIRecording : MonoBehaviour {
 	{
 		SetButton (false);
 		Events.SetRecording (false);
-//		GetComponent<GameController> ().ChangeState (GameController.states.EDITING);
 	}
 
-
-//	public void ToggleSaveButton () {		
-//		return;
-//		SetButton (!isRecording);
-//		Events.SetRecording (isRecording);
-//		if(!isRecording)
-//			GetComponent<GameController> ().ChangeState (GameController.states.EDITING);
-//	}
 
 	void SetButton(bool _isOn)
 	{
 		isRecording = _isOn;
 		if (isRecording) {
 			label.text = "GRABANDO...";
-//			button.SetActive (false);
-//			panel.SetActive (true);
-//			Events.Log ("Recording...");
 		} else {
 			label.text = "PULSA PARA GRABAR";
-//			panel.SetActive (false);
-//			Events.Log ("");
 		}
-
 	}
 }
